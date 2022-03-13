d707c1f35bbd40448c693d7ee9de8b9bad60e289
JoeC <joe.chiu>
2022-03-13 15:34:49 +1100

    init

--- kv.py
+++ kv.py
@@ -2,20 +2,25 @@
 import sys
 
 try:
-    sys.path.append('/home/joechiu/utils')
-    import u
+  sys.path.append('/home/joechiu/utils')
+  import u
 except:
-    print("utilities not found")
+  print('{"error":"utilities not found"}')
+  sys.exit()
 
 arr = list()
 kvn = "kv-box-001" # the existing key vault name in azure
-opt = sys.argv[1]
+try:
+  opt = sys.argv[1]
+except:
+  u.js.pp({"error":"option not found"})
+  sys.exit()
 
 def fill(a):
   for key in a:
-      res = u.az.kv(kvn, key)
-      h = u.js.loads(res)
-      arr.append(h)
+    res = u.az.kv(kvn, key)
+    h = u.js.loads(res)
+    arr.append(h)
 
 if opt.lower() in ['*','all']:
   fill(u.js.loads(u.az.kvlist(kvn)))
